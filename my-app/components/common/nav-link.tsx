05918f77a1c4c59e484e946abdab308b1f415ac1
innocent <terminalkarabo@gmail.com>
2025-05-09 15:01:58 +0200

    feat: enhance NavLink component to highlight active link based on current pathname

--- my-app/components/common/nav-link.tsx
+++ my-app/components/common/nav-link.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export default function NavLink({
   href,
@@ -10,10 +13,12 @@ export default function NavLink({
   children: React.ReactNode;
   className?: string;
 }>) {
+    const pathname = usePathname();
+    const isActive = pathname === href || (href !== "/" && pathname.startsWith(href));
   return (
     <Link
       href={href}
-      className={cn("no-underline text-gray-900 hover:text-blue-500 transition duration-200 ease-in-out active:text-red-500", className)}
+      className={cn("no-underline text-gray-900 hover:text-blue-500 transition duration-200 ease-in-out active:text-red-500", className, isActive && 'text-rose-400')}
     >
       {children}
     </Link>
