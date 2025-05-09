9e505487f38961a13236cf15c209d0ae33ee0427
innocent <terminalkarabo@gmail.com>
2025-05-09 18:32:55 +0200

    feat: add Badge component and enhance HeroSection with improved styling and functionality

--- my-app/components/home/hero-section.tsx
+++ my-app/components/home/hero-section.tsx
@@ -1,28 +1,44 @@
 import { Button } from "@/components/ui/button";
-import { Sparkles } from 'lucide-react';
+import { ArrowRight, Sparkles } from 'lucide-react';
+import { Badge } from "@/components/ui/badge"
+import Link from "next/link";
+
 
 export default function HeroSection() {
-    return <section>
-        <div className="">
+    return <section className="relative mx-auto flex flex-col z=0
+         items-center justify-center py-16 sm:py-20 lg:py-28
+        transition-all animate-in lg:px-12 max-w-7xl">
             <div className="flex">
-                <Sparkles className="h-6 w-6 text-green-900 animate-pulse" />
-                <p>Powerd by AI</p>
+                <Badge className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-green-500 to-blue-500 text-white font-bold text-sm flex items-center gap-2 px-3 py-1">
+                    <div className="absolute inset-0 w-full h-full bg-gradient-to-r from-green-500 to-blue-500 opacity-50 blur-[2px] animate-pulse"></div>
+                    <Sparkles className="h-6 w-6 text-black animate-pulse" />
+                    <p className="text-white">Powerd by AI</p>
+                </Badge>
             </div>
 
-            <h1>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-center text-gray-900 mt-8">
                 Summarize PDFs into key points <br />
                 and insights.
             </h1>
-            <h2>
+            <h2 className="text-lg sm:text-xl lg:text-2xl font-normal text-center text-gray-700 mt-4 max-w-3xl">
                 Upload your PDF and get a summary in seconds.
                 <br />
                 Save time and effort by quickly extracting key information from your PDFs.
             </h2>
 
-            <Button>
-                Try Opsom
+            <Button
+            asChild
+            variant="link"
+            className="mt-6 rounded-full px-6 py-3 text-base sm:text-lg lg:text-xl font-semibold text-white bg-primary hover:bg-primary/90 hover:no-underline transition duration-200 ease-in-out"
+            >
+            <Link
+                href="/#pricing"
+                className="inline-flex items-center gap-2 no-underline hover:no-underline"
+            >
+                <span>Try Opsom</span>
+                <ArrowRight className="w-5 h-5 animate-pulse" />
+            </Link>
             </Button>
-        </div>
 
     </section>
 }
