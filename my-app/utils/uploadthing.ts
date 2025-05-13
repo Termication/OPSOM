fd3a3fdfdb3ebd1b31899f18dbdf095e39b385d3
innocent <terminalkarabo@gmail.com>
2025-05-13 15:28:57 +0200

    fix: update type inference in useUploadThing export to use 'typeof ourFileRouter'

--- my-app/utils/uploadthing.ts
+++ my-app/utils/uploadthing.ts
@@ -4,4 +4,4 @@ import { ourFileRouter } from "@/app/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react";
 
 export const { useUploadThing }
-    = generateReactHelpers<ourFileRouter>();
+    = generateReactHelpers<typeof ourFileRouter>();
