4c3139b30dd3cd219228d2c8fbe92eb1e75d6102
innocent <terminalkarabo@gmail.com>
2025-05-17 15:39:46 +0200

    feat: implement PDF summary generation and text extraction functionality

--- my-app/actions/upload-actions.tsx
+++ my-app/actions/upload-actions.tsx
@@ -1,5 +1,42 @@
 'use server'
 
-export async function generatePdfSummary() {
-    
+export async function generatePdfSummary(uploadRespose: [{
+    serverData: {
+        userId: string;
+        file: {
+            url: string;
+            name: string;
+        };
+    };
+}]) {
+    if (!uploadRespose) {
+        return {
+            success: false,
+            message: "No upload response provided.",
+            data: null,
+        };
+    }
+
+    const { serverData: 
+        userId,
+        file: { url: pdfUrl, name: fileName },
+     } = uploadRespose[0];
+
+     if(!pdfUrl){
+        return {
+            success: false,
+            message: "No PDF URL provided.",
+            data: null,
+        };
+     }
+
+     try {
+        const pdfText = await extractTextFromPdf(pdfUrl);
+     } catch (err) {
+        return {
+            success: false,
+            message: "Error extracting text from PDF.",
+            data: null,
+        }
+    }
 }
---
+++ my-app/lib/langchain.tsx
@@ -0,0 +1,3 @@
+export async function extractTextFromPdf(pdfUrl: string): Promise<string> {{
+    
+}
